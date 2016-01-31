b98aaebf826a5f9c38668581338a112bb943d30a
yuji0001 <yuji0001@192.168.1.8>
2016-01-31 11:46:44 +0900

    修正

--- www/beauti/regist/controllers.js
+++ www/beauti/regist/controllers.js
@@ -8,7 +8,18 @@ angular.module('miral.beauti.regist.controllers', [])
 	//初期ビュー
 	$scope.viewNo = "1";
 
+	$scope.prop = {};
+	$scope.prop.headerColor = "calm";
 
+	$scope.getHeaderClass = function(){
+		var cl =  ["bar-subheader"];
+		cl.push("bar-"+$scope.prop.headerColor);
+		return cl;
+	}
+
+	$scope.onBasicNextBtn=function(){
+		$scope.viewNo="2";
+	}
 
 })
 
