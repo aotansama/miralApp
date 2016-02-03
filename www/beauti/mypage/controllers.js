5e0ed1f6fe512a753c31dc643b1ef100310dc5da
ekchuah <ekchuah@192.168.1.34>
2016-02-03 19:09:02 +0900

    mypageとdetailの修正

--- www/beauti/mypage/controllers.js
+++ www/beauti/mypage/controllers.js
@@ -3,6 +3,10 @@ angular.module('miral.beauti.mypage.controllers', [])
 .controller('beautiMypageControllers', function($scope, $state) {
 
 
+	  $scope.myGoBack = function() {
+		    $ionicHistory.goBack();
+		  };
+
 })
 
 ;
