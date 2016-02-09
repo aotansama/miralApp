86ebc93bc499ba4c6b3856893eef822f8d2aa4f3
pesca <pesca@192.168.1.22>
2016-02-09 14:23:22 +0900

    2月8日作業分

--- www/beauti/mypage/controllers.js
+++ www/beauti/mypage/controllers.js
@@ -6,20 +6,16 @@ angular.module('miral.beauti.mypage.controllers', [])
 	//初期ビュー
 	$scope.viewNo = "1";
 
-	$scope.prop = {};
-	$scope.prop.headerColor = "calm";
-
-	$scope.getHeaderClass = function(){
-		var cl =  ["bar-subheader"];
-		cl.push("bar-"+$scope.prop.headerColor);
-		return cl;
-	}
-
-	$scope.onBasicNextBtn=function(){
+	$scope.RpopmodifyNextBtn=function(){
 		$scope.viewNo="2";
 	}
 
-	$scope.onBasicNextBtn2=function(){
+	$scope.RpopmodifyBackBtn=function(){
+		$scope.viewNo="1";
+	}
+
+
+	$scope.RpopmodifyNextBtn2=function(){
 		$scope.viewNo="3";
 	}
 
